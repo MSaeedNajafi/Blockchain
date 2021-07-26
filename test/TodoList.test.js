9ad2ee4b546a41c7796fc2c4a39273ec8f261e54
Saeed <najafisaeed@gmail.com>
2021-07-26 17:12:14 +0200

    full todolist

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -16,26 +16,35 @@ contract("TodoList", (accounts) => {
     console.log(address);
   });
 
-  //   it("lists of tasks", async () => {
-  //     const taskCount = await this.TodoList.taskCount();
-  //     const task = await this.TodoList.tasks(taskCount);
-  //     assert.equal(task.id.toNumber(), taskCount.toNumber());
-  //     assert.equal(task.content, "contact Laywer");
-  //     assert.equal(task.completed, false);
-  //     assert.equal(taskCount.toNumber(), 1);
-  //   });
+  it("lists of tasks", async () => {
+    const taskCount = await this.TodoList.taskCount();
+    const task = await this.TodoList.tasks(taskCount);
+    assert.equal(task.id.toNumber(), taskCount.toNumber());
+    assert.equal(task.content, "contact Laywer");
+    assert.equal(task.completed, false);
+    assert.equal(taskCount.toNumber(), 1);
+  });
 
-  //   it("creats tasks", async () => {
-  //     const result = await this.TodoList.createTask(
-  //       "do something with your life!"
-  //     );
-  //     const taskCount = await this.TodoList.taskCount();
-  //     assert.equal(taskCount, 2);
-  //     const event = result.logs[0].args;
-  //     // console.log(result);
-  //     // console.log(event);
-  //     assert.equal(event.id.toNumber(), 2);
-  //     assert.equal(event.content, "do something with your life!");
-  //     assert.equal(event.completed, false);
-  //   });
+  it("creats tasks", async () => {
+    const result = await this.TodoList.createTask(
+      "do something with your life!"
+    );
+    const taskCount = await this.TodoList.taskCount();
+    assert.equal(taskCount, 2);
+    const event = result.logs[0].args;
+    // console.log(result);
+    // console.log(event);
+    assert.equal(event.id.toNumber(), 2);
+    assert.equal(event.content, "do something with your life!");
+    assert.equal(event.completed, false);
+  });
+
+  it("toggles task completion", async () => {
+    const result = await this.TodoList.toggleCompleted(1);
+    const task = await this.TodoList.tasks(1);
+    assert.equal(task.completed, true);
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 1);
+    assert.equal(event.completed, true);
+  });
 });
